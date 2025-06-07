7aa7427193b5683e95983318165d94ce1f0b7181
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-06-06 20:41:18 -0400

    update the jobdb factory to support supabase_local mode

--- src/dr_exp/utils/jobdb_factory.py
+++ src/dr_exp/utils/jobdb_factory.py
@@ -31,7 +31,7 @@ def get_job_db_client(config: Optional[JobDBConfig] = None) -> BaseJobDB:
     
     config.validate()
     
-    if config.mode == "supabase_remote":
+    if config.mode in ["supabase_remote", "supabase_local"]:
         return SupabaseJobDB(config)
     else:
         return LocalJobDB(config)
